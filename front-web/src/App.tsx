9e18e755586f94488c4e1aecaa844b135199c6fe
Thaylor Carvalho <thaylorcs@outlook.com>
2021-01-26 15:38:33 -0300

    Aula de componentização

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Alert from './Alert';
+
+const App = () => {
+    return (
+        <div className="container mt-5">
+            <Alert text="Washington"/>
+            <Alert text="Thaylor"/>
+            <Alert text="Gabi"/>
+        </div>
+    )
+}
+
+export default App;
--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
-  <h1>Hello World</h1>,
+  <App />,
   document.getElementById('root')
 );
