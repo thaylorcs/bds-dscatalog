5719db28a6ef148b8e6f97353bc3b67a2d4557dd
Thaylor Carvalho <thaylorcs@outlook.com>
2021-01-27 19:46:55 -0300

    Instalação Limpa

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,34 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import react from 'react';
 
 const App = () => {
-
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        console.log("o contador mudou de valor")
-    }, []) //Use effect utiliza uma função e um array de dependências como parâmetro se estiver vazio, só executa 1x
-
     return (
-        <div className="container mt-5">
-           <button 
-                className="btn btn-primary mr-5"
-                onClick={() => setCounter(counter + 1)}
-           >
-               +
-           </button>
-           <span>
-               {counter}
-           </span>
-           <button 
-            className="btn btn-primary ml-5"
-            onClick={() => setCounter(counter - 1)}
-           >
-               -
-           </button>
-           {counter > 5 && <h1>O valor é maior que 5</h1>}
-           {counter < 5 && <h1>O valor é menor que 5</h1>}
-        </div>
-    )
+        <h1>App</h1>
+    );
 }
 
 export default App;
