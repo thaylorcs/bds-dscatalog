0c566e8fb63e1868da56697cdeb7360f566f5881
Thaylor Carvalho <thaylorcs@outlook.com>
2021-01-27 19:44:04 -0300

    UseState e UseEffect

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import Alert from './Alert';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("o contador mudou de valor")
+    }, []) //Use effect utiliza uma função e um array de dependências como parâmetro se estiver vazio, só executa 1x
+
     return (
         <div className="container mt-5">
-            <Alert text="Washington"/>
-            <Alert text="Thaylor"/>
-            <Alert text="Gabi"/>
+           <button 
+                className="btn btn-primary mr-5"
+                onClick={() => setCounter(counter + 1)}
+           >
+               +
+           </button>
+           <span>
+               {counter}
+           </span>
+           <button 
+            className="btn btn-primary ml-5"
+            onClick={() => setCounter(counter - 1)}
+           >
+               -
+           </button>
+           {counter > 5 && <h1>O valor é maior que 5</h1>}
+           {counter < 5 && <h1>O valor é menor que 5</h1>}
         </div>
     )
 }
