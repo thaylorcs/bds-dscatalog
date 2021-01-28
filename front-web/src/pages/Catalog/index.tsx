727a1a3d58252d0c8c3a42a3450c0fdc0e8203c0
Thaylor Carvalho <thaylorcs@outlook.com>
2021-01-28 20:55:25 -0300

    Criação da Página de Produtos

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import ProductCard from './components/ProductCard'
 import './styles.scss'
 
 const Catalog = () => (
     <div className="catalog-container">
         <h1 className="catalog-title">Catálogo de produtos</h1>
         <div className="catalog-products">
-            
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
         </div>
     </div>
 );
