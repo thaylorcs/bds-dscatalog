719613184d524fbcacb2f6512f4017482cc5addd
Thaylor Carvalho <thaylorcs@outlook.com>
2021-02-05 16:06:33 -0300

    Lidando com formulários

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
 const Form = () => {
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('computadores');
+
+    const handleOnChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setName(event.target.value);
+    }
+
+    const handleOnChangePrice = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPrice(event.target.value);
+    }
+
+    const handleOnChangeCategory = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setName(event.target.value);
+    }
+
     return (
         <BaseForm title="Cadastrar um Produto">
+            <h1>Name: {name}</h1>
+            <h1>Price: {price}</h1>
             <div className="row">
                 <div className="col-6">
-                    <input type="text" className="form-control" />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        onChange={handleOnChangeName}
+                    />
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        onChange={handleOnChangePrice}
+                    />
+                    <select value={category} className="form-control mb-5" onChange={handleOnChangeCategory}>
+                        <option value="livros">Livros</option>
+                        <option value="computadores">Computadores</option>
+                        <option value="eletronicos">Eletrônicos</option>
+                    </select>
                 </div>
             </div>
         </BaseForm>
