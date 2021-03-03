6fb6d1f36b9edd2f04afef9298225e22a949ab06
Thaylor Carvalho <thaylorcs@outlook.com>
2021-03-03 14:35:48 -0300

    Special queries, Postgres compatibility

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -31,7 +31,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**" };
 	
-	private static final String[] OPERATOR_OR_ADMIN = { "/produts/**", "/categories/**"};
+	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**"};
 	
 	private static final String[] ADMIN = {"/users/**"};
 
