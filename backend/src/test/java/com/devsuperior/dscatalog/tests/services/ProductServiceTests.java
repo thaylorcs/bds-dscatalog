8b9335d09d1e103fb1cd69c7795f6fddc2b49fb8
Thaylor Carvalho <thaylorcs@outlook.com>
2021-04-21 13:49:31 -0300

    Repository Tests Examples

---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/services/ProductServiceTests.java
@@ -0,0 +1,19 @@
+package com.devsuperior.dscatalog.tests.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.ProductService;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+	
+	@InjectMocks
+	private ProductService service;
+	
+	@Mock
+	private ProductRepository repository;
+}
