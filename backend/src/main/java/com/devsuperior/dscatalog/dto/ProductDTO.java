7d67411c9c60930b8f3ed6d1c769891dd6b284d4
Thaylor Carvalho <thaylorcs@outlook.com>
2021-04-21 19:09:06 -0300

    Finishing Tests Section

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
@@ -35,6 +36,7 @@ public class ProductDTO implements Serializable{
 	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
+	@NotEmpty(message = "Produto sem categoria não é permitido")
 	private List<CategoryDTO> categories = new ArrayList<>();
 	
 	public ProductDTO() {}
