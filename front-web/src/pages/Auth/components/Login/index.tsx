227cf1b8afa58802e7b42b93b1d164b7fa1652a5
Thaylor Carvalho <thaylorcs@outlook.com>
2021-04-23 09:09:11 -0300

    Starting with react hook form

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,22 +1,37 @@
 import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {useForm} from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
 
+type FormData = {
+    email: string;
+    password: string;
+}
+
 const Login = () => {
+
+    const {register, handleSubmit} = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+    }
+
     return (
         <AuthCard title="Login">
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input 
                     type="email"
                     placeholder="Email"
                     className="form-control input-base margin-bottom-30"
+                    {...register('email')}
                 />
                 <input 
                     type="password"
                     placeholder="Senha"
                     className="form-control input-base"
+                    {...register('password')}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueceu a senha?
