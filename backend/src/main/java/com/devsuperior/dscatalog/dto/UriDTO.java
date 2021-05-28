591a314111082332a438f2b00d9f94a40286b0db
Thaylor Carvalho <thaylorcs@outlook.com>
2021-05-28 18:36:41 -0300

    Upload Endpoint

---
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UriDTO.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dscatalog.dto;
+
+public class UriDTO {
+	
+	private String uri;
+	
+	public UriDTO() {}
+	
+	public UriDTO(String uri) {
+		this.uri = uri;
+	}
+
+	public String getUri() {
+		return uri;
+	}
+
+	public void setUri(String uri) {
+		this.uri = uri;
+	}
+}
