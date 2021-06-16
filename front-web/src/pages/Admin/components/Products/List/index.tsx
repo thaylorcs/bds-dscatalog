6c98dfa66f6b5844dd639e207b80f7d56417b79d
Thaylor Carvalho <thaylorcs@outlook.com>
2021-06-16 10:59:56 -0300

    Integrate Product listing with backend

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
+import Pagination from 'core/components/Pagination';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Card from '../Card';
 
 const List = () => {
 
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
+
+    useEffect(() => {
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+
+        //iniciar o loader
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                //finalizar o loader
+                setIsLoading(false);
+            })
+    }, [activePage]);
 
     const handleCreate = () => {
         history.push('/admin/products/create');
@@ -17,11 +39,19 @@ const List = () => {
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                <Card />
-                <Card />
-                <Card />
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
+                {productsResponse && (
+                    <Pagination
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
             </div>
         </div>
-)};
+    )
+};
 
 export default List;
