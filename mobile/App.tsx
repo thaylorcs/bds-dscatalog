0348aa1639d4408bb10d57aa2e4233c553c37b2a
Thaylor Carvalho <thaylorcs@outlook.com>
2021-08-26 14:49:19 -0300

    End of Lesson 1

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.h1}>Alo devsuperior</Text>
+      <Text>Thaylor</Text>
+      <Text>Programador React Native</Text>
     </View>
-  );
-}
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#069',
+    flex: 1,
     justifyContent: 'center',
   },
-});
+  h1: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "700",
+
+  }
+})
+
+export default App;
